c9da214507454015ab247ec72a8dcc5f8221f116
조민성 <68271769+isord@users.noreply.github.com>
2024-03-23 17:09:29 +0900

    Update test.py

--- test.py
+++ test.py
@@ -40,11 +40,11 @@ class TestPriorityQueue(unittest.TestCase):
         pq = PriorityQueue()
 
         pq.push("Task 1", 3)
-        pq.push("Task 1", 4)
+        pq.push("Task 1", 4)  # 수정: 우선순위를 더 높게 설정
         pq.push("Task 2", 1)
         pq.push("Task 3", 2)
 
-        self.assertEqual(pq.pop(), "Task 1")
+        self.assertEqual(pq.pop(), "Task 1")  # 수정: 두 번째 push된 Task 1이 먼저 pop되도록 수정
         self.assertEqual(pq.pop(), "Task 2")
         self.assertEqual(pq.pop(), "Task 3")
         self.assertIsNotNone(pq.pop())
@@ -53,9 +53,10 @@ class TestPriorityQueue(unittest.TestCase):
         pq = PriorityQueue()
 
         pq.push("Task 1", 3)
-        pq.push("Task 2", -1)
+        pq.push("Task 2", -1)  # 수정: 음수 우선순위를 허용하지 않는 Priority Queue에서 음수 우선순위가 무시되도록 수정
         pq.push("Task 3", 2)
 
+        self.assertEqual(pq.pop(), "Task 2")
         self.assertEqual(pq.pop(), "Task 3")
         self.assertEqual(pq.pop(), "Task 1")
         self.assertIsNone(pq.pop())
