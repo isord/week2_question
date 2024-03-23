3d3fb4b6005e899ed56cebf702ea654ffc7ae3e1
조민성 <68271769+isord@users.noreply.github.com>
2024-03-23 17:24:50 +0900

    Update test.py

--- test.py
+++ test.py
@@ -40,13 +40,13 @@ class TestPriorityQueue(unittest.TestCase):
     pq = PriorityQueue()
 
     pq.push("Task 1", 3)
-    pq.push("Task 1", 4)
     pq.push("Task 2", 1)
     pq.push("Task 3", 2)
+    pq.push("Task 1", 4)
 
-    self.assertEqual(pq.pop(), "Task 1")
     self.assertEqual(pq.pop(), "Task 2")
     self.assertEqual(pq.pop(), "Task 3")
+    self.assertEqual(pq.pop(), "Task 1")
     self.assertIsNotNone(pq.pop())
 
   def test_priority_queue_negative_priority(self):
